b5bc4b82612d3cee7a074aa778758fe2a4305667
Mohamed KB <mohabel98@gmail,com>
2022-11-08 14:32:09 +0100

    ejercicios 32 y 33 hechos

---
+++ ejerciciosJS/ej32.js
@@ -0,0 +1,7 @@
+// let numero = 20;
+// let serie = [0, 1];
+// let x = 0;
+// for (let i = 0; i < numero ; i++) {
+//     serie.push(serie[serie.length - 1] + serie[serie.length - 2])
+//     console.log(serie); 
+// }
