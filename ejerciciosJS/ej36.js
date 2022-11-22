6de78c4f2dc2e05fab2a3c85522aff9aa93ddd5d
Mohamed KB <mohabel98@gmail,com>
2022-11-22 18:37:45 +0100

    ejercicios hechos

--- ejerciciosJS/ej36.js
+++ ejerciciosJS/ej36.js
@@ -1,6 +1,7 @@
 function dosEdadesMasAltas(array){
-    
+    return array.sort((a,b) => b-a).slice(0,2)
     }
     
 
-dosEdadesMasAltas([1, 2, 10, 8, 22, 3, 6, 12, 80, 4, 55, 19])
+let array = [1, 2, 10, 8, 22, 3, 6, 12, 80, 4, 55, 19];
+console.log(dosEdadesMasAltas(array)); 
